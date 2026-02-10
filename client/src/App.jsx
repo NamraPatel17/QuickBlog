25e6fa5400cf922e29d1efd977c036e240be1c9d
Namra Patel <namrapatel910@gmail.com>
2026-02-10 12:02:19 +0530

    Created NavBar and Header File

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import  Home  from './pages/Home'
+import  Blog  from './pages/Blog'
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route  path='/' element={<Home/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+      </Routes>
+    </div>
   )
 }
+
+export default App;
